a4adc9c7bd503abff2c1e34e3d52ffe845726dbd
LaTingLiu <a2546g@gmail.com>
2016-11-16 18:28:39 +0800

    123

--- testline.js
+++ testline.js
@@ -1,16 +1,16 @@
-function(){
+function testline()
+{
 	var width = 800;
 	var height = 500;
 	var ctrl = d3.select("body").append("svg").attr("width", width).attr("height", height);
 	d3.csv("./typhoon.csv", 
-	function(data)
-	{
-		var ln = data.length;
-		console.log(data);
-		var maxy = d3.max(data, function(d){ return d.number; });
-		var lines = d3.line().x(function(d,i){return i*(width/ln);}).y(function(d){return height-d.number*(height/500)});
-		ctrl.append("path").data([data]).attr("d", lines).attr("stroke", "red").attr("fill", "none");
-	}
-
-);
+		function(data)
+		{
+			var ln = data.length;
+			console.log(data);
+			var maxy = d3.max(data, function(d){ return d.number; });
+			var lines = d3.line().x(function(d,i){return i*(width/ln);}).y(function(d){return height-d.number*(height/500)});
+			ctrl.append("path").data([data]).attr("d", lines).attr("stroke", "red").attr("fill", "none");
+		}
+	);
 }
